ed605042aa3aa0c25ffdfe79d721ff6ad32a9f8d
Moaaz Mostafa <moaazmostafa7474@gmail.com>
2022-09-21 23:56:32 +0200

    second commit

--- app/config/config.php
+++ app/config/config.php
@@ -4,16 +4,12 @@
 if(!defined('DS')) define('DS' , DIRECTORY_SEPARATOR);// directory separator ''
 
 define('APP_PATH' , realpath(dirname(__FILE__)) . DS . '..');
-define('CONTROLLER_PATH' , 'C:\xampp\htdocs\Student Management System - Pure PHP\app\Controllers');
-define('MODELS_PATH' , 'C:\xampp\htdocs\Student Management System - Pure PHP\app\Models');
+define('CONTROLLER_PATH' , 'C:\xampp\htdocs\EStore Project\app\Controllers');
+define('MODELS_PATH' , 'C:\xampp\htdocs\EStore Project\app\Models');
 define('VIEWS_PATH' , APP_PATH . DS . 'views' . DS);
 define('TEMPLATE_PATH' , APP_PATH . DS . 'templates' . DS);
 define('CSS', '/css/');
 define('JS', '/js/');
-define('LANGUAGES_PATH' , APP_PATH . DS . 'languages' . DS);
-
-
-defined('APP_DEFAULT_LANGUAGE') ? null : define('APP_DEFAULT_LANGUAGE','en');
 
 
 defined('SESSION_NAME') ? null : define('SESSION_NAME','_ESTORE_SESSION');
@@ -27,7 +23,7 @@ defined('DATABASE_HOST_NAME')   ? null : define('DATABASE_HOST_NAME' , 'locahost
 
 defined('DATABASE_USER_NAME')   ? null : define('DATABASE_USER_NAME' , 'root');
 
-defined('DATABASE_HOST_PASSWORD')   ? null : define('DATABASE_HOST_PASSWORD' , '');
+defined('DATABASE_HOST_PASSWORD')   ? null : define('DATABASE_HOST_PASSWORD' , 'moaaz011password');
 
 defined('DATABASE_DB_NAME')   ? null : define('DATABASE_DB_NAME' , 'storedb');
 
